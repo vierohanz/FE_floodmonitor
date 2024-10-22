6fd86a31c99cf01e9c1f4f981668951570133a35
rayhan <rayhanzz772@gmail.com>
2024-10-22 11:40:07 +0700

    grafik added

--- lib/view/TabExample.dart
+++ lib/view/TabExample.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flood_monitor/utils/color.dart';
 import 'package:flutter/material.dart';
 
@@ -193,8 +194,126 @@ class StatusTerakhirTab extends StatelessWidget {
 class GrafikTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Grafik akan ditampilkan di sini.'),
+    return Container(
+      padding: EdgeInsets.all(12),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: kprimarySecond,
+            ),
+            child: Text(
+              'Curah Hujan',
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          AspectRatio(
+            aspectRatio: 2,
+            child: LineChart(
+              LineChartData(
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: [
+                      FlSpot(0, .83), // Titik pertama
+                      FlSpot(1, .84), // Titik kedua
+                      FlSpot(2, .82), // Titik ketiga
+                      FlSpot(3, .83), // Titik keempat
+                      FlSpot(4, .84), // Titik kelima
+                      FlSpot(5, .95), // Titik keenam
+                      FlSpot(6, .86), // Titik ketujuh
+                      FlSpot(7, .87), // Titik kedelapan
+                      FlSpot(8, .88), // Titik kesembilan
+                    ],
+                    color: Colors.red,
+                    gradient: LinearGradient(colors: [
+                      Colors.red,
+                      Colors.purpleAccent,
+                      Colors.lightBlueAccent
+                    ]),
+                    isCurved: false, // Garis lurus
+                    barWidth: 1, // Lebar garis
+                    // dotData: FlDotData(show: true), // Tampilkan titik
+                  ),
+                ],
+                titlesData: FlTitlesData(
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 22,
+                        getTitlesWidget: (value, meta) {
+                          switch (value.toInt()) {
+                            case 0:
+                              return Text('24');
+                            case 1:
+                              return Text('25');
+                            case 2:
+                              return Text('26');
+                            case 3:
+                              return Text('27');
+                            case 4:
+                              return Text('28');
+                            case 5:
+                              return Text('29');
+                            case 6:
+                              return Text('30');
+                            case 7:
+                              return Text('31');
+                            case 8:
+                              return Text('01');
+                          }
+                          return Text('');
+                        },
+                      ),
+                    ),
+                    leftTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 40,
+                        getTitlesWidget: (value, meta) {
+                          return Text(value.toStringAsFixed(2));
+                        },
+                      ),
+                    ),
+                    rightTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        // showTitles:
+                        //     false, // Menyembunyikan label di sebelah kanan (Y-axis)
+                        reservedSize: 10,
+                      ),
+                    ),
+                    topTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        // showTitles:
+                        //     false, // Menyembunyikan label di sebelah kanan (Y-axis)
+                        reservedSize: 10,
+                      ),
+                    )),
+                gridData: FlGridData(show: true),
+                borderData: FlBorderData(
+                  show: true,
+                  border: Border.all(color: Colors.black, width: 1),
+                ),
+                minX: 0,
+                maxX: 8,
+                minY: 0.80,
+                maxY: 0.95,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
