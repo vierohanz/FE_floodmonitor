d4d705afa2d6fcdc72c4fe25c1b2bb7c27ca5a11
rayhan <rayhanzz772@gmail.com>
2024-10-22 21:15:22 +0700

    .

---
+++ lib/views/component/status/StatusParent.dart
@@ -0,0 +1,15 @@
+import 'package:flood_monitor/views/component/status/StatusTerakhir.dart';
+import 'package:flutter/material.dart';
+
+class StatusParent extends StatelessWidget {
+  const StatusParent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        StatusTerakhirTab(),
+      ],
+    );
+  }
+}
