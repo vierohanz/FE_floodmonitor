7a5a9a18ba28d58b3346c6b17bee4ded02f4c01e
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-12-11 16:14:32 +0700

    message

--- lib/models/mapModel.dart
+++ lib/models/mapModel.dart
@@ -2,7 +2,11 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class mapModel {
   final List<String> nameLocation;
+  final List<LatLng> pointLocation;
   final List<String> imgLocation;
 
-  mapModel({required this.nameLocation, required this.imgLocation});
+  mapModel(
+      {required this.nameLocation,
+      required this.pointLocation,
+      required this.imgLocation});
 }
--- lib/views/messagePage.dart
+++ lib/views/messagePage.dart
@@ -1,66 +1,86 @@
+import 'package:flood_monitor/views/component/broadcast/ListBroadcast.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class messagePage extends StatefulWidget {
-  @override
-  _MessagePageState createState() => _MessagePageState();
-}
-
-class _MessagePageState extends State<messagePage> {
-  late int selectedRegencyId;
-  late double selectedRegencyLatitude;
-  late double selectedRegencyLongitude;
-  late String selectedRegencyName;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadSelectedRegency();
-  }
-
-  // Memuat data dari SharedPreferences
-  _loadSelectedRegency() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      selectedRegencyId = prefs.getInt('selectedRegencyId') ?? 0;
-      selectedRegencyName = prefs.getString('selectedRegencyName') ?? 'Unknown';
-      selectedRegencyLatitude =
-          prefs.getDouble('selectedRegencyLatitude') ?? 0.0;
-      selectedRegencyLongitude =
-          prefs.getDouble('selectedRegencyLongitude') ?? 0.0;
-    });
-  }
-
-  @override
+class messagePage extends StatelessWidget {
   Widget build(BuildContext context) {
+    final hp = MediaQuery.of(context).size.height;
+    final wp = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Message Page"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Selected Regency Details:",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text("ID: $selectedRegencyId"),
-            Text("Name: $selectedRegencyName"),
-            Text("Latitude: $selectedRegencyLatitude"),
-            Text("Longitude: $selectedRegencyLongitude"),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Menampilkan data dari SharedPreferences atau menyegarkan data
-                _loadSelectedRegency();
-              },
-              child: Text("Refresh Data"),
-            ),
-          ],
+      backgroundColor: Color(0xFFEBF0F7),
+      body: SingleChildScrollView(
+        controller: ScrollController(),
+        child: Container(
+          width: wp,
+          padding: EdgeInsets.only(top: 12, right: 22, left: 22, bottom: 10),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: hp * 0.02),
+                child: Column(
+                  children: [
+                    Container(
+                      width: wp,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(13),
+                          topRight: Radius.circular(13),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Daftar Broadcast",
+                            style: TextStyle(
+                              letterSpacing: 1,
+                              fontFamily: "NunitoSans",
+                              fontSize: wp * 0.045,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.add, color: Colors.white),
+                            onPressed: () {
+                              // Tambahkan logika ketika tombol "+" ditekan
+                              print("Tombol tambah ditekan");
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(
+                          top: 14, right: 14, left: 14, bottom: 14),
+                      width: wp,
+                      height: hp * 0.65,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade300,
+                            offset: Offset(4.0, 4.0),
+                            blurRadius: 5.0,
+                            spreadRadius: 1.0,
+                          ),
+                          BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 0.0,
+                            spreadRadius: 0.0,
+                          ),
+                        ],
+                      ),
+                      child: Listbroadcast(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
